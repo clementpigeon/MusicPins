4af176676230a5be2c3627b2b679501fa8a79504
Clement Pigeon <clementpigeon@gmail.com>
2013-10-07 10:45:01 -0400

    initial commit - creates Rails Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicPins::Application.config.secret_token = '2d9c10bf0981b42a14ad27beea6e6c02ac20db2aa675c360353aa355a6a4c7d2b36d0cce3a1283375e71c8d8009c28afba1229e2fc9076e0b741a14c53e2c4b3'
