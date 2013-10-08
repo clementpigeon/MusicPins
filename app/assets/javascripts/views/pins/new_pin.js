c2db6985b986dc65b51a78583f21790773a9252d
Clement Pigeon <clementpigeon@gmail.com>
2013-10-08 12:05:07 -0400

    adds second part of NewPinView form

--- app/assets/javascripts/views/pins/new_pin.js
+++ app/assets/javascripts/views/pins/new_pin.js
@@ -1,16 +1,34 @@
 MP.Views.NewPinView = Backbone.View.extend({
 
-  template: JST['pins/new'],
+  template1: JST['pins/new1'],
+  template2: JST['pins/new2'],
 
   className: 'newPin',
 
   render: function () {
 
     var that = this;
-    that.$el.html(that.template());
+    that.$el.html(that.template1());
 
     return that;
+  },
+
+  events: {
+    'click button.songSelected' : 'songSelected',
+    'submit form' : 'createPin'
+  },
+
+  songSelected: function(e){
+    e.preventDefault();
+    e.currentTarget.remove();
+    this.$el.find('form').append(this.template2());
+  },
+
+  createPin: function(e){
+    e.preventDefault();
+    console.log(e.currentTarget);
   }
+
 
 
 
