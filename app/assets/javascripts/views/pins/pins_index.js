7f6e0e9301f6746fbc2b50eff6fdfb5447f6e401
Clement Pigeon <clementpigeon@gmail.com>
2013-10-13 20:56:16 -0400

    refactors PinsIndex view to store rendered CardViews in an array

--- app/assets/javascripts/views/pins/pins_index.js
+++ app/assets/javascripts/views/pins/pins_index.js
@@ -4,24 +4,36 @@ MP.Views.PinsIndexView = Backbone.View.extend({
 
   col_index: 0,
 
+  colNumber: 4,
 
   which_col: function(){
     this.col_index++;
-    if (this.col_index > 4) this.col_index = 1;
+    if (this.col_index > this.colNumber) this.col_index = 1;
     return ('col' + this.col_index);
+  },
+
+  initialize: function(){
+    _.bindAll(this, 'addCardViewToArray');
+    this._pinCardViews = [];
+    this.collection.each(this.addCardViewToArray);
+
+  },
+
+  addCardViewToArray: function(pin){
+    var pinCardView = new MP.Views.PinCardView({ model: pin });
+    this._pinCardViews.push(pinCardView.render().$el);
   },
 
   render: function () {
     var that = this;
     that.$el.html(that.template());
 
-    that.collection.each(function(pin){
-      var pinCardView = new MP.Views.PinCardView({ model: pin });
+    _(this._pinCardViews).each(function(pinCardView){
       var colDiv = 'div.' + that.which_col();
-      that.$el.find(colDiv).append(pinCardView.render().$el);
+      that.$el.find(colDiv).append(pinCardView);
     });
-
     return that;
-  }
+  },
+
 
 });
