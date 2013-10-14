02041116537cfa969f79092b48e1026d6e134c33
Clement Pigeon <clementpigeon@gmail.com>
2013-10-13 22:35:36 -0400

    responsive layout on the main feed!

--- app/assets/javascripts/views/pins/pins_index.js
+++ app/assets/javascripts/views/pins/pins_index.js
@@ -2,21 +2,14 @@ MP.Views.PinsIndexView = Backbone.View.extend({
 
   template: JST['pins/index'],
 
-  col_index: 0,
-
-  colNumber: 4,
-
-  which_col: function(){
-    this.col_index++;
-    if (this.col_index > this.colNumber) this.col_index = 1;
-    return ('col' + this.col_index);
-  },
-
   initialize: function(){
-    _.bindAll(this, 'addCardViewToArray');
+    _.bindAll(this, 'addCardViewToArray', 'render', 'resizedBuffer', 'resized');
+
     this._pinCardViews = [];
     this.collection.each(this.addCardViewToArray);
 
+    this.currentLayout = this.widthToLayout($('body').width());
+    $(window).on("resize", this.resizedBuffer);
   },
 
   addCardViewToArray: function(pin){
@@ -24,9 +17,47 @@ MP.Views.PinsIndexView = Backbone.View.extend({
     this._pinCardViews.push(pinCardView.render().$el);
   },
 
-  render: function () {
+  resizedBuffer: function(){
+    if (this.resizeTimer) clearTimeout(this.resizeTimer);
+    this.resizeTimer = setTimeout(this.resized, 100);
+  },
+
+  resized: function(){
+    var newWidth = $('body').width();
+    var newLayout = this.widthToLayout(newWidth);
+
+    if (newLayout[0] !== this.currentLayout[0]){
+      this.render(newLayout);
+    };
+    this.currentLayout = newLayout;
+  },
+
+  widthToLayout: function(width){
+    if (width < 510) return [1, 242];
+    if (width < 750) return [2, 484];
+    if (width < 990) return [3, 726];
+    if (width < 1250) return [4, 968];
+    if (width < 1480) return [5, 1210];
+    if (width < 1720) return [6, 1452];
+    return [7, 1694];
+  },
+
+  // not easier to read refactoring
+  //
+  // widthToLayout: function(width){
+  //     var nb = parseInt(width / 242);
+  //     var modulo = width % 242;
+  //     if (modulo < 40) nb--;
+  //     return [nb, 242 * nb];
+  // },
+
+  render: function (newLayout) {
+    if (!newLayout) newLayout = this.currentLayout;
     var that = this;
     that.$el.html(that.template());
+
+    this.reactiveLayout(newLayout);
+    this.col_index = 0;
 
     _(this._pinCardViews).each(function(pinCardView){
       var colDiv = 'div.' + that.which_col();
@@ -35,5 +66,20 @@ MP.Views.PinsIndexView = Backbone.View.extend({
     return that;
   },
 
+  reactiveLayout: function(newLayout){
+    this.colNumber = newLayout[0];
+    this.$('section.feed_container').width(newLayout[1]);
+  },
+
+  which_col: function(){
+    this.col_index++;
+    if (this.col_index > this.colNumber) this.col_index = 1;
+    return ('col' + this.col_index);
+  },
+
+  remove: function() {
+      $(window).off("resize", this.resizedBuffer);
+      Backbone.View.prototype.remove.apply(this, arguments);
+  }
 
 });
