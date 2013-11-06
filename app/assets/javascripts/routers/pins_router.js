7d006649067cedeb8b55d336b845474749f05393
Clement Pigeon <clementpigeon@gmail.com>
2013-11-06 00:41:50 -0500

    most popular bands/songs no longer routes

--- app/assets/javascripts/routers/pins_router.js
+++ app/assets/javascripts/routers/pins_router.js
@@ -4,11 +4,7 @@ MP.Routers.PinsRouter = Backbone.Router.extend({
     'new': 'newPin',
     'user/:user_id': 'userFeed',
     'user/:user_id/likes': 'likedPinsFeed',
-    'user/:user_id/followed_songs': 'followedSongs',
-    'user/:user_id/followed_bands': 'followedBands',
-    // 'song/most_popular': 'mostPopularSongs',
     'song/:song_id': 'songFeed',
-    // 'band/most_popular': 'mostPopularBands',
     'band/:band_id': 'bandFeed',
     'pin/:pin_id' : 'pinFocus',
     'facebook': 'facebook_lookup',
@@ -119,72 +115,6 @@ MP.Routers.PinsRouter = Backbone.Router.extend({
       this.$rootEl.append(this.pinFocusView.render().$el);
     }
   },
-
-  mostPopularBands: function(){
-    var that = this;
-    var bands = new MP.Collections.Bands();
-
-    bands.fetch({
-      success: function(data){
-        var popularBandsView = new MP.Views.PopularBandsView({collection: bands});
-        $('#overlay').show();
-        that.$box.html(popularBandsView.render().$el)
-      }
-    });
-  },
-
-  mostPopularSongs: function(){
-    var that = this;
-    var songs = new MP.Collections.Songs();
-
-    songs.fetch({
-      success: function(data){
-        var popularSongsView = new MP.Views.PopularSongsView({collection: songs});
-        $('#overlay').show();
-        that.$box.html(popularSongsView.render().$el)
-      }
-    });
-  },
-
-  // followedSongs: function(user_id){
-  //   var that = this;
-  //   var user = new MP.Models.User({id: user_id});
-  //   user.fetch({
-  //     success: function(data){
-  //       user.set({
-  //         'song_followings': new MP.Collections.SongFollowings(user.get('song_followings'))
-  //       });
-  //       var followedSongsView = new MP.Views.FollowedSongsView({
-  //         collection: user.get('song_followings')
-  //       });
-  //       $('#overlay').show();
-  //       that.$box.html(followedSongsView.render().$el)
-  //     },
-  //     error: function(data, other, yetother){
-  //       console.log('error');
-  //     },
-  //   });
-  // },
-
-  // followedBands: function(user_id){
-  //   var that = this;
-  //   var user = new MP.Models.User({id: user_id});
-  //   user.fetch({
-  //     success: function(data){
-  //       user.set({
-  //         'band_followings': new MP.Collections.BandFollowings(user.get('band_followings'))
-  //       });
-  //       var followedBandsView = new MP.Views.FollowedBandsView({
-  //         collection: user.get('band_followings')
-  //       });
-  //       $('#overlay').show();
-  //       that.$box.html(followedBandsView.render().$el)
-  //     },
-  //     error: function(data, other, yetother){
-  //       console.log('error');
-  //     },
-  //   });
-  // },
 
   facebook_lookup: function(){
     var that = this;
